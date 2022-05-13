803889f6fa8960d44c20b28e39cfeb9b18fdd5e5
mauro sala <mauro.sala@gmail.com>
2022-05-13 09:48:06 +0200

    feat(chart): helm shared library

--- routes/status.routes.js
+++ routes/status.routes.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/ping', (req, res) => {
-  res.status(200).send('Template Service')
+  res.status(200).send('Endpoint Service')
 })
 
 router.get('/healthz', (req, res) => {
-  res.status(200).send('Template Service')
+  res.status(200).send('Endpoint Service')
 })
 
 module.exports = router
